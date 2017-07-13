b4838e1837bff6c5d91c4185fa089588c7850b1b
betamee <gongxq95@gmail.com>
2017-07-13 23:34:43 +0800

    设计完开发目录架构

--- server/app.js
+++ server/app.js
@@ -7,13 +7,10 @@ var bodyParser = require('body-parser');
 var AV = require('leanengine');
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
-require('./cloud');
+require('./cloud/cloud.js');
 
 var app = express();
 
-// 设置模板引擎
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
@@ -31,12 +28,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/', function(req, res) {
-  res.render('index', { currentTime: new Date() });
-});
 
 // 可以将一类的路由单独保存在一个文件中
-app.use('/todos', require('./routes/todos'));
+app.use('/', require('./server/index.js'));
 
 app.use(function(req, res, next) {
   // 如果任何一个路由都没有返回响应，则抛出一个 404 异常给后续的异常处理器
@@ -68,10 +62,7 @@ app.use(function(err, req, res, next) {
     // 如果是开发环境，则将异常堆栈输出到页面，方便开发调试
     error = err;
   }
-  res.render('error', {
-    message: err.message,
-    error: error
-  });
+  res.send(err);
 });
 
 module.exports = app;
