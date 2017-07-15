c962fdf168695ba0e519ccb63371468ad2fa4e85
betamee <gongxq95@gmail.com>
2017-07-15 21:17:19 +0800

    完成热加载热更新功能环境搭建

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,7 @@ module.exports = {
   ],
   // output.publicPath 表示资源的发布地址，当配置过该属性后，打包文件中所有通过相对路径引用的资源都会被配置的路径所替换
   output: {
-    path: path.resolve(__dirname, '/build/client'), // 与APP_PATH一致的文件目录
+    path: path.resolve(__dirname, 'app/client'), // 与APP_PATH一致的文件目录
     filename: 'devClient.bundle.js',
     publicPath: '/',
   },
@@ -53,31 +53,31 @@ module.exports = {
           },
         ],
       },
-      { // 处理图片，name属性是输出的图片文件地址，在output目录下生成图片目录以及文件
-        test: /\.(png|jpg|gif|webp')$/,
-        //  include:path.resolve(__dirname,'/client/assets'),
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 10000,
-            name: 'bundleImg/[hash:8].[name].[ext]',
-          },
-        }],
-      },
+      // { // 处理图片，name属性是输出的图片文件地址，在output目录下生成图片目录以及文件
+      //   test: /\.(png|jpg|gif|webp')$/,
+      //   //  include:path.resolve(__dirname,'/client/assets'),
+      //   use: [{
+      //     loader: 'url-loader',
+      //     options: {
+      //       limit: 10000,
+      //       name: 'bundleImg/[hash:8].[name].[ext]',
+      //     },
+      //   }],
+      // },
 
-      { // 处理字体
-        test: /\.(woff|ttf|svg|eot|woff2)$/,
-        //  include:path.resolve(__dirname,'/client/assets'),
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              name: 'bundleFonts/[hash:8]-[name].[ext]',
-            },
-          },
-        ],
-      },
+      // { // 处理字体
+      //   test: /\.(woff|ttf|svg|eot|woff2)$/,
+      //   //  include:path.resolve(__dirname,'/client/assets'),
+      //   use: [
+      //     {
+      //       loader: 'url-loader',
+      //       options: {
+      //         limit: 10000,
+      //         name: 'bundleFonts/[hash:8]-[name].[ext]',
+      //       },
+      //     },
+      //   ],
+      // },
     ],
   },
 
